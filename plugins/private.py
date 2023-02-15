e02cbe4558dbffa59b1d3671dd76e0f87b689ad4
LOGESHBOTS <122679985+LOGESHBOTS@users.noreply.github.com>
2023-02-15 06:58:39 +0530

    Update private.py

--- plugins/private.py
+++ plugins/private.py
@@ -15,7 +15,7 @@ logger = logging.getLogger(__name__)
 
 channel = UPDATE_CHANNEL
 
-ft = f"Due To Overload Only Channel Sub Are Use This Bot Join @KPSLink."
+ft = f"Due To Overload Only Channel Sub Are Use This Bot Join @urlshortenlink."
 
 
 # Private Chat
